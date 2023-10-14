ea33c3806167786fe4558fddd822e9903911e841
Antoine2803 <82040673+Antoine2803@users.noreply.github.com>
2023-10-14 12:00:21 +0200

    bouton petit ecran fonctionnel

--- second_test/script.js
+++ second_test/script.js
@@ -1,12 +1,9 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navbar = document.querySelector('.navbar');
-const navUnToggle = document.querySelector('untogglenav');
 
 navToggle.addEventListener('click', () => {
     navbar.classList.toggle('active');
+    navToggle.classList.toggle('active');
 });
 
-navUnToggle.addEventListener('click', () => {
-    navbar.classList.toggle('none');
-});
 
