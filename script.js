2524619f25ac09637c15cc5aeb89579f6df43744
Antoine2803 <82040673+Antoine2803@users.noreply.github.com>
2023-10-19 17:13:39 +0200

    careerdev upgrade

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const navToggleOn = document.querySelector('.nav-toggleOn');
 const navToggleOff = document.querySelector('.nav-toggleOff');
 const navbar = document.querySelector('.navbar');
+const frenchCV = document.querySelector('frenchCV');
+const englCV = document.querySelector('englCV');
+const frcv = document.querySelector('frCV');
+const encv = document.querySelector('enCV');
+
 
 navToggleOn.addEventListener('click', () => {
     navbar.classList.toggle('active');
@@ -12,4 +17,14 @@ navToggleOff.addEventListener('click', () => {
     navbar.classList.toggle('active');
     navToggleOn.classList.toggle('active');
     navToggleOff.classList.toggle('active');
+});
+
+encv.addEventListener('change', () =>{
+    frenchCV.classList.toggle('active' [false]);
+    englCV.classList.toggle('active' [false]);
+});
+
+frcv.addEventListener('change', () =>{
+    frenchCV.classList.toggle('active' [true]);
+    englCV.classList.toggle('active' [true]);
 });
