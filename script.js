8a9b2fc4e0cedfb2536e9c9bd3ab8c0b478cb287
Antoine2803 <82040673+Antoine2803@users.noreply.github.com>
2023-10-19 19:37:31 +0200

    boutons fix dans career_dev et nouveau cv

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const navToggleOn = document.querySelector('.nav-toggleOn');
 const navToggleOff = document.querySelector('.nav-toggleOff');
 const navbar = document.querySelector('.navbar');
-const frenchCV = document.querySelector('frenchCV');
-const englCV = document.querySelector('englCV');
-const frcv = document.querySelector('frCV');
-const encv = document.querySelector('enCV');
+const frenchCV = document.querySelector('.frenchCV');
+const englCV = document.querySelector('.englCV');
+const frcv = document.querySelector('.frCV');
+const encv = document.querySelector('.enCV');
 
 
 navToggleOn.addEventListener('click', () => {
@@ -19,12 +19,20 @@ navToggleOff.addEventListener('click', () => {
     navToggleOff.classList.toggle('active');
 });
 
-encv.addEventListener('change', () =>{
-    frenchCV.classList.toggle('active' [false]);
-    englCV.classList.toggle('active' [false]);
+encv.addEventListener('click', () =>{
+    frenchCV.classList.toggle('active');
+    englCV.classList.toggle('active');
+    frcv.classList.toggle('radon');
+    encv.classList.toggle('radon');
+    encv.disabled = true;
+    frcv.disabled = false;
 });
 
-frcv.addEventListener('change', () =>{
-    frenchCV.classList.toggle('active' [true]);
-    englCV.classList.toggle('active' [true]);
+frcv.addEventListener('click', () =>{
+    frenchCV.classList.toggle('active');
+    englCV.classList.toggle('active');
+    encv.classList.toggle('radon');
+    frcv.classList.toggle('radon');
+    frcv.disabled = true;
+    encv.disabled = false;
 });
