069cc44f733e6dca27aecf7ee867a2380aaab211
Antoine2803 <82040673+Antoine2803@users.noreply.github.com>
2023-11-01 16:55:26 +0100

    bouton dl et CV fr

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const frenchCV = document.querySelector('.frenchCV');
 const englCV = document.querySelector('.englCV');
 const frcv = document.querySelector('.frCV');
 const encv = document.querySelector('.enCV');
+const frdl = document.querySelector('.dlfr');
+const endl = document.querySelector('.dleng');
 
 navToggleOn.addEventListener('click', () => {
     navbar.classList.toggle('active');
@@ -20,7 +22,9 @@ navToggleOff.addEventListener('click', () => {
 
 encv.addEventListener('click', () =>{
     frenchCV.classList.toggle('active');
-    englCV.classList.toggle('active');
+    englCV.classList.toggle('active');    
+    endl.classList.toggle('active');
+    frdl.classList.toggle('active');
     frcv.classList.toggle('radon');
     encv.classList.toggle('radon');
     encv.disabled = true;
@@ -30,6 +34,8 @@ encv.addEventListener('click', () =>{
 frcv.addEventListener('click', () =>{
     frenchCV.classList.toggle('active');
     englCV.classList.toggle('active');
+    endl.classList.toggle('active');
+    frdl.classList.toggle('active');
     encv.classList.toggle('radon');
     frcv.classList.toggle('radon');
     frcv.disabled = true;
